8c84a55a0f30a023f8a0708cf8007a9a810b4983
Christoffer <bob_sten@hotmail.com>
2022-03-04 12:06:13 +0100

    hey igen

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hej</h1>
-  );
+    return (
+        <>
+            <h1>Hej</h1>
+            <p>Hej igen</p>
+        </>
+    );
 }
 
 export default App;
