63aef8a426c64a49e8e477669101718366f597fb
Christoffer <bob_sten@hotmail.com>
2022-03-04 12:50:25 +0100

    fra bunden

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App, Fod } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <div className="test2">
-        <App />
-        <Fod />
-    </div>,
 
     document.getElementById("root")
 
